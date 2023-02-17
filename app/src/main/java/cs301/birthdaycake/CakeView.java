99305559b1027f5421eb97c006691a1f42bccb2d
tabra26 <tabra26@up.edu>
2023-02-16 17:30:29 -0800

    team1

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -53,7 +53,7 @@ public class CakeView extends SurfaceView {
         setWillNotDraw(false);
 
         //Setup our palette
-        cakePaint.setColor(0xFFC755B5);  //violet-red
+        cakePaint.setColor(Color.RED);  //violet-red
         cakePaint.setStyle(Paint.Style.FILL);
         frostingPaint.setColor(0xFFFFFACD);  //pale yellow
         frostingPaint.setStyle(Paint.Style.FILL);
